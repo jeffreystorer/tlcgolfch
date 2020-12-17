b7e9f40bced7902d8cc31a16c62e72e0a5b616f8
jstorer <jeffrey.storer@gmail.com>
2020-12-17 11:10:24 -0500

    12/17 ok to or

--- src/components/IndividualTableHeader.js
+++ src/components/IndividualTableHeader.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React from "react"
 
-function IndividualTableHeader({tableName}) {
-    return (
+function IndividualTableHeader({ tableName }) {
+  return (
     <>
-        <tr className='individual-header-row'>
-            <th
-                scope='col'
-                className='individual-left-header-cell'
-            >
-            <div className='center'>{tableName}</div>  
-            </th>
-            <th className='individual-other-header-cell' scope='col'>DC</th>
-            <th className='individual-other-header-cell' scope='col'>MG</th>
-            <th className='individual-other-header-cell' scope='col'>MW</th>
-            <th className='individual-other-header-cell' scope='col'>OK</th>
-            <th className='individual-other-header-cell' scope='col'>PA</th>
-            <th className='individual-other-header-cell' scope='col'>TP</th>
-        </tr>
+      <tr className="individual-header-row">
+        <th scope="col" className="individual-left-header-cell">
+          <div className="center">{tableName}</div>
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          DC
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          MG
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          MW
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          OR
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          PA
+        </th>
+        <th className="individual-other-header-cell" scope="col">
+          TP
+        </th>
+      </tr>
     </>
-    )
+  )
 }
-export default IndividualTableHeader;
+export default IndividualTableHeader
